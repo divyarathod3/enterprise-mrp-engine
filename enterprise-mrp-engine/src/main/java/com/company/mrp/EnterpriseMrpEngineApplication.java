c3b96bb93f9759ae5c3b58670989fbb3a1ffef24
Divya Rathod <rathoddivya3002@gmail.com>
2026-07-10 21:50:02 +0530

    Initial Spring Boot setup for Enterprise MRP Engine

---
+++ enterprise-mrp-engine/src/main/java/com/company/mrp/EnterpriseMrpEngineApplication.java
@@ -0,0 +1,13 @@
+package com.company.mrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseMrpEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseMrpEngineApplication.class, args);
+	}
+
+}
