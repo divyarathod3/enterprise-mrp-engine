d0e91035f67bb39c74affad23aac7887f73b35a0
Divya Rathod <rathoddivya3002@gmail.com>
2026-07-13 22:36:36 +0530

    Refactored package structure and completed Item service layer

---
+++ backend/src/main/java/com/company/mrp/controller/ItemController.java
@@ -0,0 +1,50 @@
+package com.company.mrp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.mrp.entity.Item;
+import com.company.mrp.service.ItemService;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping
+    public Item addItem(@RequestBody Item item) {
+        return itemService.addItem(item);
+    }
+
+    @GetMapping
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public Item getItemById(@PathVariable Long id) {
+        return itemService.getItemById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Item updateItem(@PathVariable Long id,
+                           @RequestBody Item item) {
+        return itemService.updateItem(id, item);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteItem(@PathVariable Long id) {
+        itemService.deleteItem(id);
+    }
+}
