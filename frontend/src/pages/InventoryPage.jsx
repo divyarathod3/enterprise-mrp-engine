75d2d2cb39130e9eca935dc0ba4798959957297a
Divya Rathod <rathoddivya3002@gmail.com>
2026-07-21 14:56:29 +0530

    Created inventory management page

---
+++ frontend/src/pages/InventoryPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InventoryPage() {
+    return (
+        <div style={{padding:"30px"}}>
+            <h1>Inventory Management</h1>
+        </div>
+    );
+}
+
+export default InventoryPage;
