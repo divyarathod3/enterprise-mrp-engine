551d313241a0ba6dcab15e43adce90d96388e8f6
Divya Rathod <rathoddivya3002@gmail.com>
2026-07-21 14:54:47 +0530

    Implemented inventory service for REST API communication

---
+++ frontend/src/services/itemService.js
@@ -0,0 +1,7 @@
+import API from "../api/itemApi";
+
+export const getItems = () => API.get("/items");
+
+export const addItem = (item) => API.post("/items", item);
+
+export const deleteItem = (id) => API.delete(`/items/${id}`);
