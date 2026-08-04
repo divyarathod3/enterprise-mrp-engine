b84c4df9d190a9f1bd560ce25be380ed2860056c
Divya Rathod <rathoddivya3002@gmail.com>
2026-08-04 11:24:29 +0530

    Created SalesOrder DTO and updated repository

---
+++ backend/src/main/java/com/company/mrp/dto/SalesOrderRequest.java
@@ -0,0 +1,50 @@
+package com.company.mrp.dto;
+
+import java.time.LocalDate;
+
+public class SalesOrderRequest {
+
+    private Long customerId;
+
+    private Long itemId;
+
+    private Integer quantity;
+
+    private LocalDate orderDate;
+
+    public SalesOrderRequest() {
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+}
--- backend/src/main/java/com/company/mrp/repository/CustomerRepository.java
+++ backend/src/main/java/com/company/mrp/repository/CustomerRepository.java
@@ -2,9 +2,12 @@ package com.company.mrp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.company.mrp.entity.Customer;
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Customer findTopByOrderByIdDesc();
+
+    Optional<Customer> findByCustomerCode(String customerCode);
 
 }
